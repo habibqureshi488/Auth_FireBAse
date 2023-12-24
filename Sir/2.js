06b01a0ad98cb3b59085321154a942b442f93cdf
Habib <129363789+habibqureshi488@users.noreply.github.com>
2023-12-24 11:16:17 +0500

    Update 2.js

--- Sir/2.js
+++ Sir/2.js
@@ -1,3 +1,5 @@
 https://classroom.google.com/c/NjM5MjcxMTk1NjUz?cjc=bjlavlf
 https://chat.whatsapp.com/L0L3CGXdkksEeyQBYI9zuA
 https://react-sysborg-clone.web.app/
+
+https://complete-react-by-zubair.vercel.app/home  (React )Course PAid Free 
